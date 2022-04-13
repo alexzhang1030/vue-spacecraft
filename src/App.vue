97a8e87edf62fc358dffa6ab332964782037ba81
alexzhang1030 <1642114555@qq.com>
2022-04-13 17:40:55 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts">
+import { useGameState } from './composables/useGameState';
+useGameState()
+</script>
 <template>
   <main p="x-4 y-10" text="center gray-700 dark:gray-200">
     <router-view />
     <Footer />
   </main>
 </template>
-<script setup lang="ts">
-import { useGameState } from './composables/useGameState';
-useGameState()
-</script>

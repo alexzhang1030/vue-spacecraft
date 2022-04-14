9eab0e89f31864ec3dfa226dcdedcb317a725a0d
alexzhang <1642114555@qq.com>
2022-04-14 18:57:50 +0800

    chore: usePower

--- src/composables/usePower.ts
+++ src/composables/usePower.ts
@@ -1,22 +1,26 @@
 import { promiseTimeout } from '@vueuse/core'
 
-const powerDisabled = ref<boolean>(false)
+const powerDisabled = ref<boolean>(true)
 
 watch(
   () => powerDisabled.value,
-  async(newVal) => {
+  async (newVal) => {
     if (!newVal) {
-      await promiseTimeout(1000)
-      powerDisabled.value = true
+      await promiseTimeout(5000)
+      powerDisabled.value = !powerDisabled.value
     }
   },
+  {
+    immediate: true
+  }
 )
 
 export const usePower = () => {
   useEventListener(document, 'keydown', (evt) => {
     const { code } = evt
-    if (code === 'Enter')
-      powerDisabled.value = true
+    if (code === 'Enter' && powerDisabled.value) {
+      powerDisabled.value = false
+    }
   })
   return powerDisabled
 }
